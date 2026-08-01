eb99c452dc3a022666836a2d1ca9cf1ff0e29066
Muhammed Niyas <mdniyas.vp@gmail.com>
2026-08-01 18:53:05 +0530

    fix: remove invalid preprocessor import and clean model pipeline

--- src/pipelines/model_pipeline.py
+++ src/pipelines/model_pipeline.py
@@ -1,14 +1,12 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from src.data.preprocessing import preprocessor
 
 from src.models.registry import MODELS
 
-
 categorical_features = [
     "Geography",
-    "Gender"
+    "Gender",
 ]
 
 numerical_features = [
@@ -19,78 +17,29 @@ numerical_features = [
     "NumOfProducts",
     "HasCrCard",
     "IsActiveMember",
-    "EstimatedSalary"
+    "EstimatedSalary",
 ]
-
 
 preprocessor = ColumnTransformer(
     transformers=[
         (
             "categorical",
             OneHotEncoder(handle_unknown="ignore"),
-            categorical_features
+            categorical_features,
         ),
         (
             "numerical",
             StandardScaler(),
-            numerical_features
-        )
-    ]
-)
-
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-
-from src.models.registry import MODELS
-
-
-categorical_features = [
-    "Geography",
-    "Gender"
-]
-
-numerical_features = [
-    "CreditScore",
-    "Age",
-    "Tenure",
-    "Balance",
-    "NumOfProducts",
-    "HasCrCard",
-    "IsActiveMember",
-    "EstimatedSalary"
-]
-
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        (
-            "categorical",
-            OneHotEncoder(handle_unknown="ignore"),
-            categorical_features
+            numerical_features,
         ),
-        (
-            "numerical",
-            StandardScaler(),
-            numerical_features
-        )
     ]
 )
 
 
-def build_pipeline(model_name: str):
-
-    return Pipeline(
-        steps=[
-            ("preprocessor", preprocessor),
-            ("classifier", MODELS[model_name])
-        ]
-    )
 def build_pipeline(
     model_name: str,
     model=None,
 ):
-
     classifier = model if model is not None else MODELS[model_name]
 
     return Pipeline(
